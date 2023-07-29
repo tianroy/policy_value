0a8efe16bd4418b421b01919f62306171e77eb08
tianroy <madwoo@gmail.com>
2023-07-29 16:23:37 +0800

    add annual premium increase rate

--- valuation.py
+++ valuation.py
@@ -25,7 +25,7 @@ def policy_valuation(data, starting_age, gender, num_simulations, risk_factor, p
             if np.random.rand() <= death_probability:
                 ages_of_death.append(age)
                 pv_death_benefit = policy_size / ((1 + discount_rate) ** (age - starting_age))
-                pv_premiums = sum([annual_premium / ((1 + discount_rate) ** i) for i in range(age - starting_age)])
+                pv_premiums = sum([annual_premium *((1 + annual_premium_increase) ** i) / ((1 + discount_rate) ** i) for i in range(age - starting_age)])
                 pv_values.append(pv_death_benefit - pv_premiums)
                 break
             age += 1
@@ -42,6 +42,7 @@ st.text(f'Policy Size: {policy_size} USD')
 gender = st.selectbox('Gender', options=['male', 'female'])
 starting_age = st.slider('Age', min_value=0, max_value=110, value=80, step=1)
 annual_premium = st.slider('Annual Premium, eg: +2 means you pay 2 USD to policy owner per year', min_value=-10.0, max_value=20.0, value=1.8, step=0.1)
+annual_premium_increase = st.slider('Annual Premium Increase %, 12 means today you pay 2 usd the next year you pay 2.24 and so on', min_value=0.0, max_value=30.0, value=12, step=1) / 100
 discount_rate = st.slider('Discount Rate %, 5.3 means 5.3%', min_value=0.0, max_value=20.0, value=5.3, step=0.1) / 100
 risk_factor = st.slider('Risk Factor, eg: 1.1 means 10% more chance of dying each year comparing with average US person', min_value=0.5, max_value=1.5, value=1.0, step=0.1)
 num_simulations = st.slider('Number of Simulations, small number will speed up the calculation', min_value=1000, max_value=10000, value=4000, step=500)
